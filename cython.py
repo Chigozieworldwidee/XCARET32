9981cec91ac181e9def4bec4007311376f2bcf30
YBMK <106607309+YBMK@users.noreply.github.com>
2022-05-31 16:43:03 +0100

    .

--- cython.py
+++ cython.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("cython").mulai()
+	__import__("cython").FUCK()
 elif 'aarch' in arc:
-	__import__("cythonn").mulai()
+	__import__("cythonn").FUCK()
 else:
 	exit(f' Unknow device machine {arc}')
